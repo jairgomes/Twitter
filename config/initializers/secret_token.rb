8679f025491fee5e0f7a4c00a5e9d1d07d7b614c
Jair G¢mez <jair_5247@hotmail.com>
2013-09-18 19:41:54 -0600

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = 'd7718a3287627e9c9b89809df5eefa8ee985262f2d33c294223123b94ba8a7e662867a4266e61bdd601249467f6c224e95fcfc090abf3a6c7aba33ca08f9d5a3'
